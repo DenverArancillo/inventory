feac09f67cc69cf65a745c06b25e19a879de70e7
Denver Arancillo <denver.arancillo@gmail.com>
2020-08-18 23:44:02 +0800

    add logout route

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,9 +47,12 @@
                         </ul>
                         <ul class="nav navbar-top-links navbar-right">
                             <li>
-                                <a href="login.html">
+                                <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                     <i class="fa fa-sign-out"></i> Log out
                                 </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
                             </li>
                         </ul>
                     </div>
