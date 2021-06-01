1d13bbc71f32684e8070fb25e009416b3c5f6ced
Denver Arancillo <denver.arancillo@gmail.com>
2021-06-02 00:28:37 +0800

    add user factory for user data generation

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
@@ -23,10 +24,11 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
+            'last_name' => 'Arncillo',
+            'first_name' => 'Denver',
+            'email' => 'sample@sample.com',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('qwe.123'),
             'remember_token' => Str::random(10),
         ];
     }
