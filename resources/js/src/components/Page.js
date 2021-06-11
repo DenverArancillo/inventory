fe26945829210d3bab65d338f655d9938b0f4607
Denver Arancillo <denver.arancillo@gmail.com>
2021-06-11 23:55:10 +0800

    update page component to hide header action if user is not logged in

--- resources/js/src/components/Page.js
+++ resources/js/src/components/Page.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react';
 import Header from './Header';
 
+import Cookies from '../components/Cookies';
+
 const Page = ({ title, headerAction, content }) => {
 
     return (
@@ -9,7 +11,11 @@ const Page = ({ title, headerAction, content }) => {
 			<div className="container w-screen mx-auto p-9 mt-4">
 				<div className="bg-white p-4 border-t border-gray-200 rounded-t-lg shadow overflow-hidden flex items-center justify-between">
 					<h3 className="text-lg leading-6 font-medium text-gray-900">{(!!title) ? title : ''}</h3>
-					{(!!headerAction) ? headerAction() : null}
+					{
+						!!Cookies.get('access_token')
+						? !!headerAction ? headerAction() : null
+						: null
+					}
 				</div>
 				{(!!content) ? content : null}
 			</div>
