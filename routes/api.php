a1f70bdb995a8c3d00ea57c051e808c80f32ab95
Denver Arancillo <denver.arancillo@gmail.com>
2021-07-04 23:03:21 +0800

    add product type routes

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
-use App\Http\Controllers\BrandsController;
-use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+use App\Http\Controllers\BrandsController;
+use App\Http\Controllers\ProductTypesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,14 +25,17 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/brands', [BrandsController::class, 'index']);
+Route::get('product-types', [ProductTypesController::class, 'index']);
 
 // Protected routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
+	Route::post('/logout', [AuthController::class, 'logout']);
+
 	Route::post('/brands', [BrandsController::class, 'store']);
 	Route::get('/brands/{id}', [BrandsController::class, 'show']);
 	Route::put('/brands/{id}', [BrandsController::class, 'update']);
 	Route::delete('/brands/{id}', [BrandsController::class, 'destroy']);
 	Route::get('/brands/search/{name}', [BrandsController::class, 'search']);
 
-	Route::post('/logout', [AuthController::class, 'logout']);
+	Route::post('/product-types', [ProductTypesController::class, 'store']);
 });
