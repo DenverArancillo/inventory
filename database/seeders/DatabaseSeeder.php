7bdef4916d29df6627ee7ee9b82c79a0988329c2
Denver Arancillo <denver.arancillo@gmail.com>
2021-07-04 23:02:57 +0800

    update database seeder to include product type

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            UserSeeder::class,
             BrandSeeder::class,
-            UserSeeder::class
+            ProductTypeSeeder::class
         ]);
     }
 }
