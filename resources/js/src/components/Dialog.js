93f5def6b5e6b311afd787d96cdd354beb0fef40
Denver Arancillo <denver.arancillo@gmail.com>
2021-07-04 23:04:42 +0800

    update dialog container to be above opacity element

--- resources/js/src/components/Dialog.js
+++ resources/js/src/components/Dialog.js
@@ -5,7 +5,7 @@ const Dialog = ({ title, content, state, handleDialog }) => {
             <div className={`transition-opacity duration-100 fixed w-full h-full top-0 left-0 flex items-center justify-center ${(state) ? '' : 'opacity-0 pointer-events-none'}`}>
                 <div className="absolute w-full h-full bg-gray-900 opacity-50"></div>
                 
-                <div className="bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+                <div className="bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-10 overflow-y-auto">
                     <div className="px-4 py-3 flex items-center justify-between">
                         <div className="text-left text-lg leading-6 font-medium text-gray-900">
                             {title}
