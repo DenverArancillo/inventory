e0d2f1eb99a9b95c6f2702708fcde9a6343d5b7d
Denver Arancillo <denver.arancillo@gmail.com>
2021-08-30 20:59:23 +0800

    finalize product types

--- resources/js/src/pages/ProductTypes.js
+++ resources/js/src/pages/ProductTypes.js
@@ -32,7 +32,7 @@ const ProductTypes = ({ match }) => {
     // add brand dialog input
     const productType = useRef();
     // edit brand dialog input
-    const editDialogBrandNameRef = useRef();
+    const editDialogProductTypeRef = useRef();
 
     const addDialogContent = handleDialog => {
         const submitNewBrand = async event => {
@@ -83,102 +83,101 @@ const ProductTypes = ({ match }) => {
         );
     }
 
-    // const editDialogEvent = (event, data) => {
-    //     editDialogBrandNameRef.current.value = data.brand_name;
+    const editDialogEvent = (event, data) => {
+        editDialogProductTypeRef.current.value = data.type;
 
-    //     setStateEditDialogData({
-    //         open: true,
-    //         content: { ...data }
-    //     });
-    // }
+        setStateEditDialogData({
+            open: true,
+            content: { ...data }
+        });
+    }
 
-    // const editDialogContent = (handleDialog) => {
-    //     const submitEditBrand = async event => {
-    //         event.preventDefault();
+    const editDialogContent = (handleDialog) => {
+        const submitEditBrand = async event => {
+            event.preventDefault();
 
-    //         if (editDialogBrandNameRef.current.value === editDialogData.content.brand_name) return;
-    //         if (editDialogBrandNameRef.current.value === '') {
-    //             setStateEditDialogErrorMessage('Brand Name is required.');
-    //             return;
-    //         }
+            if (editDialogProductTypeRef.current.value === editDialogData.content.type) return;
+            if (editDialogProductTypeRef.current.value === '') {
+                setStateEditDialogErrorMessage('Brand Name is required.');
+                return;
+            }
 
-    //         setStateLoading(true);
+            setStateLoading(true);
 
-    //         let brand_name = editDialogBrandNameRef.current.value;
-    //         let response = await fetchApi('put', `/api/brands/${editDialogData.content.id}`, { brand_name });
+            let type = editDialogProductTypeRef.current.value;
+            let response = await fetchApi('put', `/api/product-types/${editDialogData.content.id}`, { type });
 
-    //         if (response.data.status) {
-    //             editDialogBrandNameRef.current.form.reset();
-    //             setStateBrands(response.data.brands);
-    //             handleDialog(false);
-    //             setStateLoading(false);
-    //             setStateEditDialogErrorMessage('');
-    //         } else {
-    //             setStateLoading(false);
-    //             setStateEditDialogErrorMessage(response.data.message);
-    //         }
-    //     }
+            if (response.data.status) {
+                editDialogProductTypeRef.current.form.reset();
+                setStateProductTypes(response.data.productTypes);
+                handleDialog(false);
+                setStateLoading(false);
+                setStateEditDialogErrorMessage('');
+            } else {
+                setStateLoading(false);
+                setStateEditDialogErrorMessage(response.data.message);
+            }
+        }
 
-    //     return (
-    //         <form>
-    //             <div className={`text-sm text-red-500 mb-2 ${!editDialogErrorMessage ? 'hidden': ''}`}>{editDialogErrorMessage}</div>
-    //             <div className="flex flex-col mb-3">
-    //                 <label>Brand Name</label>
-    //                 <input required ref={editDialogBrandNameRef} className="my-2 p-1 bg-gray-200 rounded border-2 border-gray-200 focus:outline-none focus:border-blue-400" type="text" id="brandName"/>
-    //             </div>
-    //             <div className="flex items-center justify-end">
-    //                 <button className="bg-blue-400 rounded-md border-2 border-blue-400 text-white p-1 text-base focus:outline-none" type="submit" onClick={submitEditBrand}>Submit</button>
-    //             </div>
-    //         </form>
-    //     )
-    // }
+        return (
+            <form>
+                <div className={`text-sm text-red-500 mb-2 ${!editDialogErrorMessage ? 'hidden': ''}`}>{editDialogErrorMessage}</div>
+                <div className="flex flex-col mb-3">
+                    <label>Product type</label>
+                    <input required ref={editDialogProductTypeRef} className="my-2 p-1 bg-gray-200 rounded border-2 border-gray-200 focus:outline-none focus:border-blue-400" type="text" id="productType"/>
+                </div>
+                <div className="flex items-center justify-end">
+                    <button className="bg-blue-400 rounded-md border-2 border-blue-400 text-white p-1 text-base focus:outline-none" type="submit" onClick={submitEditBrand}>Submit</button>
+                </div>
+            </form>
+        )
+    }
 
-    // const deleteDialogEvent = (event, data) => {
-    //     setStateDeleteDialogData({
-    //         open: true,
-    //         content: { ...data }
-    //     })
-    // }
+    const deleteDialogEvent = (event, data) => {
+        setStateDeleteDialogData({
+            open: true,
+            content: { ...data }
+        })
+    }
 
-    // const deleteDialogContent = (handleDialog) => {
-    //     const deleteBrand = async event => {
-    //         event.preventDefault();
+    const deleteDialogContent = (handleDialog) => {
+        const deleteBrand = async event => {
+            event.preventDefault();
 
-    //         setStateLoading(true);
+            setStateLoading(true);
 
-    //         let brand_name = editDialogBrandNameRef.current.value
-    //         let response = await fetchApi('delete', `/api/brands/${deleteDialogData.content.id}`, { brand_name });
+            let response = await fetchApi('delete', `/api/product-types/${deleteDialogData.content.id}`);
 
-    //         if (response.data.status) {
-    //             setStateBrands(response.data.brands);
-    //             handleDialog(false);
-    //             setStateLoading(false);
-    //         }
-    //     }
+            if (response.data.status) {
+                setStateProductTypes(response.data.productTypes);
+                handleDialog(false);
+                setStateLoading(false);
+            }
+        }
 
-    //     const cancelDialog = (event) => {
-    //         event.preventDefault();
-    //         handleDialog(false);
-    //     }
+        const cancelDialog = (event) => {
+            event.preventDefault();
+            handleDialog(false);
+        }
 
-    //     return (
-    //         <form>
-    //             <div className="flex flex-col mb-5">
-    //                 {/* optional chaining, content property is not set on state */}
-    //                 Delete brand {deleteDialogData?.content?.brand_name}?
-    //             </div>
-    //             <div className="flex items-center justify-center">
-    //                 <button className="bg-blue-400 rounded-md border-2 border-blue-400 text-white p-1 text-base focus:outline-none mr-2" type="submit" onClick={deleteBrand}>Delete</button>
-    //                 <button className="rounded-md border-2 p-1 text-base focus:outline-none ml-2" type="submit" onClick={cancelDialog}>Cancel</button>
-    //             </div>
-    //         </form>
-    //     )
-    // }
+        return (
+            <form>
+                <div className="flex flex-col mb-5">
+                    {/* optional chaining, content property is not set on state */}
+                    Delete brand {deleteDialogData?.content?.type}?
+                </div>
+                <div className="flex items-center justify-center">
+                    <button className="bg-blue-400 rounded-md border-2 border-blue-400 text-white p-1 text-base focus:outline-none mr-2" type="submit" onClick={deleteBrand}>Delete</button>
+                    <button className="rounded-md border-2 p-1 text-base focus:outline-none ml-2" type="submit" onClick={cancelDialog}>Cancel</button>
+                </div>
+            </form>
+        )
+    }
 
     return (
         <Fragment>
             {/* dialogs */}
-            {/* <Dialog
+            <Dialog
                 title="Edit Product type"
                 content={editDialogContent}
                 state={editDialogData.open}
@@ -189,7 +188,7 @@ const ProductTypes = ({ match }) => {
                 content={deleteDialogContent}
                 state={deleteDialogData.open}
                 handleDialog={setStateDeleteDialogData}
-            /> */}
+            />
 
             <Loading state={loading}/>
             <Page
@@ -203,10 +202,10 @@ const ProductTypes = ({ match }) => {
                             { title: 'ID', id: 'id', style: "" },
                             { title: 'Name', id: 'type', style: "" }
                         ]}
-                        // actions={[
-                        //     { title: "Edit", icon: () => <PencilAltIcon className="h-4 w-4 focus:outline-none"/>, event: editDialogEvent },
-                        //     { title: "Delete", icon: () => <TrashIcon className="h-4 w-4 focus:outline-none"/> , event: deleteDialogEvent },
-                        // ]}
+                        actions={[
+                            { title: "Edit", icon: () => <PencilAltIcon className="h-4 w-4 focus:outline-none"/>, event: editDialogEvent },
+                            { title: "Delete", icon: () => <TrashIcon className="h-4 w-4 focus:outline-none"/> , event: deleteDialogEvent },
+                        ]}
                     />
                 )}
             />
